2c7cec70e8e1d5a355a3c7ca052a9a4d51e72e92
jack-avery <email.jack.avery@gmail.com>
2023-05-25 15:51:02 -0400

    move disabled

--- scripts/sourcemod.sh
+++ scripts/sourcemod.sh
@@ -1,5 +1,6 @@
 METAMOD=https://mms.alliedmods.net/mmsdrop/1.11/mmsource-1.11.0-git1148-linux.tar.gz
 SOURCEMOD=https://sm.alliedmods.net/smdrop/1.11/sourcemod-1.11.0-git6934-linux.tar.gz
+test -e build || mkdir build
 
 # download latest sourcemod
 curl -o ./build/sourcemod.tar.gz $SOURCEMOD
@@ -9,6 +10,13 @@ tar xvf sourcemod.tar.gz
 # merge repo in
 cd ..
 cp -r -u ./tf/addons/sourcemod/* ./build/addons/sourcemod
+
+# move disabled plugins
+test -e ./build/addons/sourcemod/scripting/disabled || mkdir ./build/addons/sourcemod/scripting/disabled 
+cat ./build/addons/sourcemod/scripting/disabled.txt | while read line
+do
+  mv "./build/addons/sourcemod/scripting/$line.sp" "./build/addons/sourcemod/scripting/disabled"
+done
 
 # compile
 cd ./build/addons/sourcemod/scripting
