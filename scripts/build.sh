ceb81904c681e20f372996ab90377731421dfd01
jack-avery <email.jack.avery@gmail.com>
2023-05-25 17:11:23 -0400

    attempt allow build using both windows and linux

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,41 +1,58 @@
-METAMOD=https://mms.alliedmods.net/mmsdrop/1.11/mmsource-1.11.0-git1148-linux.tar.gz
-SOURCEMOD=https://sm.alliedmods.net/smdrop/1.11/sourcemod-1.11.0-git6934-linux.tar.gz
+METAMOD=https://mms.alliedmods.net/mmsdrop/1.11/mmsource-1.11.0-git1148
+SOURCEMOD=https://sm.alliedmods.net/smdrop/1.11/sourcemod-1.11.0-git6934
+MMOUT=metamod
+SMOUT=sourcemod
+
+if [[ "$1" = "win" ]]; then
+  SUFFIX=-windows.zip
+  COMPILE=./compile.exe
+  dos2unix ./tf/addons/sourcemod/scripting/disabled.txt
+else
+  SUFFIX=-linux.tar.gz
+  COMPILE=./compile.sh
+fi
+
+METAMOD=$METAMOD$SUFFIX
+SOURCEMOD=$SOURCEMOD$SUFFIX
+MMOUT=$MMOUT$SUFFIX
+SMOUT=$SMOUT$SUFFIX
 
 # clear old build
-test -e build || rm -r build
+rm -r build
 mkdir build
 
-# download latest metamod
-curl -o ./build/metamod.tar.gz $METAMOD
+# download metamod
+curl -o ./build/$MMOUT $METAMOD
 cd ./build
-tar xvf metamod.tar.gz
+unzip $MMOUT
+cd ..
 
-# download latest sourcemod
-curl -o ./build/sourcemod.tar.gz $SOURCEMOD
+# download sourcemod
+curl -o ./build/$SMOUT $SOURCEMOD
 cd ./build
-tar xvf sourcemod.tar.gz
+unzip $SMOUT
+cd ..
 
 # merge repo in
-cd ..
 cp -r -u ./tf/addons/sourcemod/* ./build/addons/sourcemod
 
 # move disabled plugins
-test -e ./build/addons/sourcemod/scripting/disabled || mkdir ./build/addons/sourcemod/scripting/disabled 
-cat ./build/addons/sourcemod/scripting/disabled.txt | while read line
+test -e ./build/addons/sourcemod/scripting/disabled || mkdir ./build/addons/sourcemod/scripting/disabled
+cat ./tf/addons/sourcemod/scripting/disabled.txt | while read line
 do
-  mv "./build/addons/sourcemod/scripting/$line.sp" "./build/addons/sourcemod/scripting/disabled"
+  mv ./build/addons/sourcemod/scripting/$line.sp ./build/addons/sourcemod/scripting/disabled
 done
 
 # compile
 cd ./build/addons/sourcemod/scripting
-./compile.sh
+$COMPILE
 cd ../../../..
 
 # move compiled plugins
 mv ./build/addons/sourcemod/scripting/compiled/* ./build/addons/sourcemod/plugins
 
 # clean up
-rm ./build/*.tar.gz
+rm ./build/*$SUFFIX
 
 # move in current config
 cp -r ./tf/* ./build
